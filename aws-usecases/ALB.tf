db994715c01fcef081525bfde814d0dd7dff3733
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-25 18:21:33 +0530

    fix

--- aws-usecases/ALB.tf
+++ aws-usecases/ALB.tf
@@ -1,13 +1,9 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c1d7eb198a14f4b7"
+  ami           = "ami-08df646e18b182346"
   instance_type = "t2.micro"
   user_data = <<EOF
-  
   echo "sudo apt update && sudo apt install apache2 -y" > file.sh
   sh file.sh
-  # echo $(curl http://169.254.169.254/latest/meta-data/local-ipv4) > /var/www/html/index.html
-  # sudo systemctl restart ngnix
-
   EOF
 
   tags = {
