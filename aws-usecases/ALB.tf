1915c1a026d39a05bcee8d47dcae205119b3545b
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-25 17:45:34 +0530

    fix

--- aws-usecases/ALB.tf
+++ aws-usecases/ALB.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "app_server" {
   user_data = <<EOF
   sudo apt update -y
   sudo apt install nginx -y
-  echo $(curl http://169.254.169.254/latest/meta-data/local-ipv4) > /var/www/html/index.html
-  sudo systemctl restart ngnix
+  # echo $(curl http://169.254.169.254/latest/meta-data/local-ipv4) > /var/www/html/index.html
+  # sudo systemctl restart ngnix
 
   EOF
 
