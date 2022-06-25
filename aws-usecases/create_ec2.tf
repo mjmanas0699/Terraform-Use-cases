9c44fe18071230a3379bac9a998c36000b6165ad
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-25 16:33:39 +0530

    fix

--- aws-usecases/create_ec2.tf
+++ aws-usecases/create_ec2.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0113c5df2bf208a04"
   instance_type = "t2.micro"
 
   tags = {
