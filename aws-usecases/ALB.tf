a7d5dfd571a81576345f20e617aa6b65a942f4af
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-25 18:46:31 +0530

    fix

--- aws-usecases/ALB.tf
+++ aws-usecases/ALB.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "app_server" {
   ami           = "ami-08df646e18b182346"
   instance_type = "t2.micro"
   user_data = <<EOF
-  echo "sudo apt update && sudo apt install apache2 -y" > file.sh
-  sh file.sh
+  sudo amazon-linux-extras install nginx1 -y
+  echo $(curl http://169.254.169.254/latest/meta-data/local-ipv4) > /usr/share/nginx/html/index.html
+  sudo systemctl start ngnix
   EOF
 
   tags = {
