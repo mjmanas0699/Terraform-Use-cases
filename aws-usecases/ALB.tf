0db98bd2254ecf67167b42a74f289a95d3751ab0
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-26 13:17:03 +0530

    added new resource

--- aws-usecases/ALB.tf
+++ aws-usecases/ALB.tf
@@ -15,6 +15,14 @@ EOF
     Name = "Terraform"
   }
 }
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["ap-south-1a","ap-south-1b","ap-south-1c"]
+
+}
+
 resource "aws_lb_target_group" "server" {
   name     = "example-lb-tg"
   port     = 80
@@ -27,4 +35,14 @@ resource "aws_lb_target_group_attachment" "test" {
   target_id        = aws_instance.app_server.id
   port             = 80
   depends_on       = [aws_lb_target_group.server]
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
 }
