08754cb3446e0dfde06320dd273384d3f0c9418e
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-26 13:19:39 +0530

    fix

--- aws-usecases/ALB.tf
+++ aws-usecases/ALB.tf
@@ -43,6 +43,6 @@ resource "aws_lb_listener" "front_end" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.test.arn
+    target_group_arn = aws_lb_target_group.server.arn
   }
 }
