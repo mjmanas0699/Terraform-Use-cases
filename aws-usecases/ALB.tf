b5f3c6d1092b425d6edc2103bf44134e48b4fcf0
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-06-26 13:22:22 +0530

    fix subnet

--- aws-usecases/ALB.tf
+++ aws-usecases/ALB.tf
@@ -19,7 +19,7 @@ resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  subnets            = ["ap-south-1a","ap-south-1b","ap-south-1c"]
+  subnets            = ["subnet-02b3dd105b1fb0e33","subnet-0fff2c4a5a7501ba8","subnet-0b31198f213e44561"]
 
 }
 
