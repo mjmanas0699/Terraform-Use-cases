ab4ad922a24128dcd5d352d382805ce2229d67a2
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-07-02 01:56:21 +0530

    added eks cluster creation

---
+++ aws-usecases/Eks_with_autoscaling.tf
@@ -0,0 +1,26 @@
+resource "aws_eks_cluster" "cluster" {
+  name     = "Autoscaler"
+  role_arn = "arn:aws:iam::376604405359:role/eksClusterRole"
+
+  vpc_config {
+    subnet_ids = ["subnet-02b3dd105b1fb0e33","subnet-0fff2c4a5a7501ba8","subnet-0b31198f213e44561"]
+  }
+
+#   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+#   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+#   depends_on = [
+#     aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+#     aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+#   ]
+    kubernetes_network_config {
+        service_ipv4_cidr ="192.168.1.0/24"
+    }
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
