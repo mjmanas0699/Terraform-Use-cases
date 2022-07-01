d005d477c21c3dc7d45ef40e264c8b515a7130e6
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-07-02 02:40:42 +0530

    added addons

--- aws-usecases/Eks_with_autoscaling.tf
+++ aws-usecases/Eks_with_autoscaling.tf
@@ -24,3 +24,15 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name      = aws_eks_cluster.cluster.name
+  addon_name        = "vpc-cni"
+  depends_on        =[aws_eks_cluster.cluster]
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name      = aws_eks_cluster.cluster.name
+  addon_name        = "kube-proxy"
+  depends_on        =[aws_eks_cluster.cluster]
+}
