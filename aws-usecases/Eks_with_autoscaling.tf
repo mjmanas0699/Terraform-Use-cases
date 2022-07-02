ddda21d3f0164f370708fc454806529e714edf17
Manas Jain <43311295+mjmanas0699@users.noreply.github.com>
2022-07-03 01:55:12 +0530

    added roles and policies

--- aws-usecases/Eks_with_autoscaling.tf
+++ aws-usecases/Eks_with_autoscaling.tf
@@ -33,6 +33,8 @@ resource "aws_eks_addon" "kube_proxy" {
 resource "aws_eks_addon" "coredns" {
   cluster_name      = aws_eks_cluster.cluster.name
   addon_name        = "coredns"
+  addon_version     = "v1.8.7-eksbuild.1"
+  resolve_conflicts = "OVERWRITE"
   depends_on        =[aws_eks_cluster.cluster]
 }
 
@@ -55,5 +57,75 @@ resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = []
   url             = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
-  depends_on        =[aws_eks_cluster.cluster]
+}
+
+resource "aws_iam_role" "role" {
+  name = "AmazonEKSClusterAutoscalerRole"
+
+  assume_role_policy = jsonencode(
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Federated": "${aws_iam_openid_connect_provider.cluster.arn}"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+            "${aws_iam_openid_connect_provider.cluster.url}:sub": "system:serviceaccount:kube-system:cluster-autoscaler"
+          }
+        }
+      }
+    ]
+  }
+  )
+}
+
+
+resource "aws_iam_policy" "policy" {
+  name        = "AmazonEKSClusterAutoscalerPolicy"
+  description = "Autoscaling Policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode(
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "aws:ResourceTag/k8s.io/cluster-autoscaler/${aws_eks_cluster.cluster.name}": "owned"
+                }
+            }
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeAutoScalingGroups",
+                "ec2:DescribeLaunchTemplateVersions",
+                "autoscaling:DescribeTags",
+                "autoscaling:DescribeLaunchConfigurations"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
---
+++ installation.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# Installing Terraform 
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sleep 1
+sudo apt-get update && sudo apt-get install terraform
+
+#Installing AWS-CLI
+sudo apt install zip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#Configure AWS
+aws configure
